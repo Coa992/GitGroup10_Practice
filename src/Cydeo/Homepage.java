7fbe6bfeabee13e4f44a240c145d108968432a57
Aleksandar <kaslu1992@gmail.com>
2024-01-23 13:35:40 -0600

    Created Homepage

---
+++ src/Cydeo/Homepage.java
@@ -0,0 +1,13 @@
+package Cydeo;
+
+public class Homepage {
+    public static void main(String[] args) {
+
+
+        //setting up homepage
+
+        //checking all the functions
+
+        //enabling Git functions
+    }
+}
