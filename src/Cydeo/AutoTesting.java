0b5a88b75d15ce13b0f053a462916ce55fc26784
Aleksandar <kaslu1992@gmail.com>
2024-01-23 14:06:51 -0600

    Automation testing part started

---
+++ src/Cydeo/AutoTesting.java
@@ -0,0 +1,16 @@
+package Cydeo;
+
+public class AutoTesting {
+    public static void main(String[] args) {
+
+        // we are done with Java for now
+
+       //Automation is starting
+
+
+       // real practice starts now
+
+
+
+    }
+}
