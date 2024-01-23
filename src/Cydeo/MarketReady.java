3f9ce89184cb661bb5a24fcba0fd021b4a71c0cf
Aleksandar <kaslu1992@gmail.com>
2024-01-23 14:17:46 -0600

    Interview preparation

---
+++ src/Cydeo/MarketReady.java
@@ -0,0 +1,13 @@
+package Cydeo;
+
+public class MarketReady {
+    public static void main(String[] args) {
+
+
+        //Interview preparation started
+
+       //praciticing soft skills
+
+
+    }
+}
