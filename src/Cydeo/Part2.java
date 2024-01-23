c3fb7973ca480a47cce1d7f5f192b80ac1dbd478
Aleksandar <kaslu1992@gmail.com>
2024-01-23 14:02:04 -0600

    Part 2 started

---
+++ src/Cydeo/Part2.java
@@ -0,0 +1,13 @@
+package Cydeo;
+
+public class Part2 {
+    public static void main(String[] args) {
+
+        //part2 course has started
+
+        //new menthor has been assigned
+
+        //same group members
+
+    }
+}
