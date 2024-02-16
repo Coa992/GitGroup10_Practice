a1bc680180d00b95f2c8d95b54b8781ef00d4cbf
mohammedalsaadi <mohammeddawood272@gmail.com>
2024-02-16 10:29:23 -0600

    Updated Week2 Tasks

--- src/Mohammed/week2/PrintConsecutiveNumbers.java
+++ src/Mohammed/week2/PrintConsecutiveNumbers.java
@@ -3,9 +3,30 @@ package Mohammed.week2;
 public class PrintConsecutiveNumbers {
     public static void main(String[] args) {
 
-        int number = 0;
+        int N = 30;
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0 && i % 3 == 0) {
+                System.out.println("CodilityTest");
+            } else if (i % 2 == 0 && i % 5 == 0) {
+                System.out.println("CodilityCoders");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("TestCoders");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else {
+                System.out.println(i);
+            }
+        }
     }
 }
+
 
 
 /*
