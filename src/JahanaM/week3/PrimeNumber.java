b1ad377bb490f54b81098208fe5d7a043aade165
jahan <jahan.mamedova91@gmail.com>
2024-02-23 12:56:58 -0600

    created a class prime numbers

---
+++ src/JahanaM/week3/PrimeNumber.java
@@ -0,0 +1,19 @@
+package JahanaM.week3;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPrime(13));
+        System.out.println(isPrime(17));
+    }
+
+
+
+
+    public static boolean isPrime(int n){
+        for(int i = 2;i<n;i++)
+            if (n%i==0){
+                return false;
+            }
+        return true;
+    }
+}
