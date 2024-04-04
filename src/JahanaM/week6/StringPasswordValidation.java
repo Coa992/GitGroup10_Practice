82673280636e37689c4ab5ee7ee9d17cebf6746b
jahan <jahan.mamedova91@gmail.com>
2024-04-04 09:17:50 -0500

    Created class Min number of array

---
+++ src/JahanaM/week6/StringPasswordValidation.java
@@ -0,0 +1,10 @@
+package JahanaM.week6;
+
+import java.util.Scanner;
+
+public class StringPasswordValidation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print your password");
+    }
+}
