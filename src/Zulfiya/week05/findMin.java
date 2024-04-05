a6190b0860f5179c49a0dbfb6788b5185c4cada7
ZulfiyaCydeo <ztazriyeva@gmail.com>
2024-04-05 14:36:07 -0400

    Merge branch 'master' into Zulfiya

---
+++ src/Zulfiya/week05/findMin.java
@@ -0,0 +1,26 @@
+package Zulfiya.week05;
+
+public class findMin {
+    public static void main(String[] args) {
+        System.out.println( findMin( new int[]{32, 5, 6, 4}));
+    }
+
+//    Question1: Array - Find Minimum
+//    Write a method that can find the minimum number from an int Array
+ public static  int findMin(int[] numbers){
+
+     int min = numbers[0];
+
+     for (int eachElement : numbers) {
+         if(eachElement<min){
+             min=eachElement;
+         }
+
+     }
+    return min;
+    }
+
+//    Question3: Array - Sort Descending
+//    Write a return method that can sort an int array in descending order without using the sort method of the Arrays class Ex: int[] arr = {10,20,7, 8, 90};
+//    arr = Sort(arr); ==> {90, 20, 10, 8, 7};
+}
