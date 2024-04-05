b6236d17e1c3514e025c582bb5908bd9af1abe86
Lisa <dyatka11@gmail.com>
2024-04-05 09:54:37 -0500

    6th week of tasks is done

---
+++ src/Lisa/week7/task2/SortAscending.java
@@ -0,0 +1,27 @@
+package Lisa.week7.task2;
+
+import java.util.Arrays;
+
+public class SortAscending {
+    public static void main(String[] args) {
+        int[] arr = {9, 3, 7, 1};
+        asc(arr);
+    }
+
+    public static void asc(int[] arr) {
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int hold = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = hold;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = arr[i];
+        }
+        System.out.println(Arrays.toString(result));
+    }
+}
