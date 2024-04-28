6259398be7ea36c3e1022514d7d5538f2b9f966f
Lisa <dyatka11@gmail.com>
2024-04-28 15:34:53 -0500

    11th week is done

--- src/Lisa/week10/task3/MapSortByValues.java
+++ src/Lisa/week10/task3/MapSortByValues.java
@@ -4,35 +4,34 @@ import java.util.*;
 
 public class MapSortByValues {
     public static void main(String[] args) {
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-
+        Map<Integer, Integer> map = new TreeMap<>();
         map.put(3, 10);
         map.put(2, 70);
         map.put(9, 60);
-        map.put(1, 10);
+        map.put(1, 90);
         map.put(7, 50);
-        map.put(0, 30);
+        map.put(8, 30);
+
+
         SortMap(map);
-        System.out.println(map);
+
 
     }
 
     public static void SortMap(Map<Integer, Integer> map) {
-
-        List<Integer> sortedValues = new ArrayList<>(map.values());
-
-        Map<Integer, Integer> sortedMap = new LinkedHashMap<>();
-
-        Collections.sort(sortedValues);
-
-        int indexOfList = 0;
-
-        for (Map.Entry<Integer, Integer> eachEntrySet : map.entrySet()) {
-            sortedMap.put(eachEntrySet.getKey(), eachEntrySet.setValue(sortedValues.get(indexOfList)));
-            indexOfList++;
-
+        Map<Integer, Integer> swappedMap = new TreeMap<>();//maintains ascending order
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            swappedMap.put(entry.getValue(), entry.getKey());
         }
+        System.out.println(swappedMap);
+
+        Map<Integer, Integer> sortedByValue=new LinkedHashMap<>();//keeps the insertion order
+        for (Map.Entry<Integer, Integer> eachEntry : swappedMap.entrySet()) {
+            sortedByValue.put(eachEntry.getValue(), eachEntry.getKey());
+        }
+        System.out.println(sortedByValue);
     }
+
 }
 
 
